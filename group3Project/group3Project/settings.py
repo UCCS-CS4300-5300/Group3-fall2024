f75e514aa3e8ebdd9e0a12ebe072495168aa8d33
Ditmanson <97914433+Ditmanson@users.noreply.github.com>
2024-10-08 22:18:19 +0000

    added get csv to script

--- group3Project/group3Project/settings.py
+++ group3Project/group3Project/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+
+from decouple import config
+
+TMDB_API_KEY = config('TMDB_API_KEY')
+TMDB_READ_ONLY_ACCESS_TOKE('TMDB_READ_ONLY_ACCESS_TOKEN')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
